54c29db3a5abf3ce3a422969e283d3c3f1bebf02
trishhhh13 <trishala13kohad@gmail.com>
2021-09-01 23:02:30 +0530

    Updated Final Commit

--- app/src/main/java/com/trishala13kohad/yourtube/MainActivity.kt
+++ app/src/main/java/com/trishala13kohad/yourtube/MainActivity.kt
@@ -15,48 +15,67 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.android.volley.Request
 import com.android.volley.toolbox.JsonObjectRequest
-import org.w3c.dom.Text
-import java.lang.Exception
 
 class MainActivity : AppCompatActivity(), VideoClicked {
 
+    //Video adapter to update views of video details
     private lateinit var mAdapter: VideoAdapter
     private lateinit var pb: ProgressBar
+
+    //next page button
     private lateinit var next: ImageView
+
+    //previous page button
     private lateinit var previous: ImageView
     private lateinit var recyclerView: RecyclerView
-    private lateinit var textView: TextView
-    private lateinit var imageView: ImageView
+
+    //textView and imageView when no data is found
+    private lateinit var foundNothing: TextView
+    private lateinit var nothingFoundImage: ImageView
+
+    //keyword to search for
     private lateinit var keyword: TextView
+
+    //to get the next 50 pages
     private lateinit var pageToken: String
     private lateinit var pageNo: TextView
+
+    //to save unit numbers
     private var pointer = 0
     private var point = 0
-    var pg = 1
+    private var pg = 1
+
+    //to find when the next and previous button is clicked for the first time or not
     private var nextClick = false
     private var prevClick = false
+
+    //api key to be used
     private val apiKey = BuildConfig.YOU_KEY
+
     //Arraylist to store the details of the video
     private val videosArray = ArrayList<VideoDetails>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
 
         previous = findViewById(R.id.previous)
         next = findViewById(R.id.next)
         pb = findViewById(R.id.spinner)
         pageNo = findViewById(R.id.pageNo)
+
         //Setting recycler view layout
         recyclerView = findViewById(R.id.recyclerView)
         recyclerView.layoutManager = LinearLayoutManager(this)
-        textView = findViewById(R.id.found_nothing)
-        imageView = findViewById(R.id.found_nothing_image)
 
+        foundNothing = findViewById(R.id.found_nothing)
+        nothingFoundImage = findViewById(R.id.found_nothing_image)
 
         keyword = findViewById<EditText>(R.id.inputKeyword)
+
         val search = findViewById<ImageView>(R.id.searchIcon)
 
+        //searching when the last button that is imeOptions button is clicked
         keyword.setOnEditorActionListener { v, actionId, event ->
             if (actionId == EditorInfo.IME_ACTION_SEARCH) {
                 if (keyword.text.toString() != "" && keyword.text.toString().trim() != "") {
@@ -71,6 +90,7 @@ class MainActivity : AppCompatActivity(), VideoClicked {
             }
             return@setOnEditorActionListener false
         }
+
         //Setting onClickListener for e item in the recycler view
         search.setOnClickListener {
             if (keyword.text.toString() != "" && keyword.text.toString().trim() != "") {
@@ -83,6 +103,7 @@ class MainActivity : AppCompatActivity(), VideoClicked {
             }
         }
 
+        //Going to previous page when clicked previous button
         previous.setOnClickListener {
             println("$point space $pointer")
             if (point >= 10) {
@@ -93,16 +114,14 @@ class MainActivity : AppCompatActivity(), VideoClicked {
                 if (keyword.text.toString() != "" && keyword.text.toString().trim() != "") {
                     searchByKeyword(keyword.text.toString())
                 }
-            }
-            else if(pointer >= 10 && !prevClick){
+            } else if (pointer >= 10 && !prevClick) {
                 pointer -= 10
-                pg-=1
+                pg -= 1
                 pageNo.text = pg.toString()
                 if (keyword.text.toString() != "" && keyword.text.toString().trim() != "") {
                     searchFromKeyword(keyword.text.toString())
                 }
-            }
-            else if (pointer == 40){
+            } else if (pointer == 40) {
                 prevClick = false
                 pg -= 1
                 pageNo.text = pg.toString()
@@ -110,41 +129,43 @@ class MainActivity : AppCompatActivity(), VideoClicked {
                     searchFromKeyword(keyword.text.toString())
                 }
 
-            }
-            else {
-                Toast.makeText(this, "Click on > to go to next page",
-                    Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(
+                    this, "Click on > to go to next page",
+                    Toast.LENGTH_SHORT
+                ).show()
             }
         }
+
+        //going to next page ehen clicked next button
         next.setOnClickListener {
             if (pointer <= 39) {
                 nextClick = true
                 pointer += 10
-                pg+=1
+                pg += 1
                 pageNo.text = pg.toString()
-                if(keyword.text.toString() != "" && keyword.text.toString().trim() != "") {
+                if (keyword.text.toString() != "" && keyword.text.toString().trim() != "") {
                     searchFromKeyword(keyword.text.toString())
                 }
-            }
-            else if(point == 0 && nextClick){
+            } else if (point == 0 && nextClick) {
                 nextClick = false
-                pg+=1
+                pg += 1
                 pageNo.text = pg.toString()
-                if(keyword.text.toString() != "" && keyword.text.toString().trim() != "") {
+                if (keyword.text.toString() != "" && keyword.text.toString().trim() != "") {
                     searchByKeyword(keyword.text.toString())
                 }
-            }
-            else if(point <= 39){
+            } else if (point <= 39) {
                 point += 10
-                pg+=1
+                pg += 1
                 pageNo.text = pg.toString()
-                if(keyword.text.toString() != "" && keyword.text.toString().trim() != "") {
+                if (keyword.text.toString() != "" && keyword.text.toString().trim() != "") {
                     searchByKeyword(keyword.text.toString())
                 }
-            }
-            else {
-                Toast.makeText(this, "Looks like you've reached the end",
-                    Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(
+                    this, "Looks like you've reached the end",
+                    Toast.LENGTH_SHORT
+                ).show()
             }
         }
 
@@ -153,13 +174,15 @@ class MainActivity : AppCompatActivity(), VideoClicked {
         recyclerView.adapter = mAdapter
     }
 
+
+    //function called for first five pages as we can get only 50 response at a time
     @SuppressLint("SetTextI18n")
     private fun searchFromKeyword(word: String) {
 
         //Showing progress bas as the search begins
         pb.visibility = View.VISIBLE
-        textView.visibility = View.GONE
-        imageView.visibility = View.GONE
+        foundNothing.visibility = View.GONE
+        nothingFoundImage.visibility = View.GONE
         recyclerView.scrollToPosition(0)
 
         //Youtube url for searching list
@@ -224,19 +247,16 @@ class MainActivity : AppCompatActivity(), VideoClicked {
 
                     //making progressbar go away
                     pb.visibility = View.GONE
-                }
-                catch(e: Exception){
+                } catch (e: Exception) {
 
                     videosArray.clear()
                     next.visibility = View.GONE
                     previous.visibility = View.GONE
                     recyclerView.visibility = View.GONE
-                    textView.visibility = View.VISIBLE
-                    imageView.visibility = View.VISIBLE
+                    foundNothing.visibility = View.VISIBLE
+                    nothingFoundImage.visibility = View.VISIBLE
                     pb.visibility = View.GONE
                     pageNo.visibility = View.GONE
-
-
 
                 }
             },
@@ -245,14 +265,14 @@ class MainActivity : AppCompatActivity(), VideoClicked {
         MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest)
     }
 
-
+    //function to call next 50 pages
     @SuppressLint("SetTextI18n")
     private fun searchByKeyword(word: String) {
 
         //Showing progress bas as the search begins
         pb.visibility = View.VISIBLE
-        textView.visibility = View.GONE
-        imageView.visibility = View.GONE
+        foundNothing.visibility = View.GONE
+        nothingFoundImage.visibility = View.GONE
         recyclerView.scrollToPosition(0)
 
         //Youtube url for searching list
@@ -318,18 +338,16 @@ class MainActivity : AppCompatActivity(), VideoClicked {
 
                     //making progressbar go away
                     pb.visibility = View.GONE
-                }
-                catch(e: Exception){
+                } catch (e: Exception) {
 
                     videosArray.clear()
                     next.visibility = View.GONE
                     previous.visibility = View.GONE
                     recyclerView.visibility = View.GONE
-                    textView.visibility = View.VISIBLE
-                    imageView.visibility = View.VISIBLE
+                    foundNothing.visibility = View.VISIBLE
+                    nothingFoundImage.visibility = View.VISIBLE
                     pageNo.visibility = View.GONE
                     pb.visibility = View.GONE
-
 
 
                 }
@@ -339,26 +357,28 @@ class MainActivity : AppCompatActivity(), VideoClicked {
         MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest)
     }
 
+    //handling the back button click
+    //not to jump out of the app instantly from any page
     override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
-        return if (keyCode == KeyEvent.KEYCODE_BACK && pointer>=10) {
+        return if (keyCode == KeyEvent.KEYCODE_BACK && pointer >= 10) {
             pointer -= 10
-            pg-=1
+            pg -= 1
             pageNo.text = pg.toString()
             searchFromKeyword(keyword.text.toString())
             true
-        }
-        else if(keyCode == KeyEvent.KEYCODE_BACK && point >=10) {
-                point -= 10
+        } else if (keyCode == KeyEvent.KEYCODE_BACK && point >= 10) {
+            point -= 10
             pg -= 1
             pageNo.text = pg.toString()
-                searchByKeyword(keyword.text.toString())
-                true
-            }
-        else
-        super.onKeyDown(keyCode, event)
+            searchByKeyword(keyword.text.toString())
+            true
+        } else
+            super.onKeyDown(keyCode, event)
     }
 
-    private fun likesAndViews(id: String){
+    /*getting response from the url using videoId in order to get
+     the views and likes of the particular video*/
+    private fun likesAndViews(id: String) {
         val urli =
             "https://youtube.googleapis.com/youtube/v3/videos?part=statistics&" +
                     "id=$id&key=$apiKey"
@@ -373,14 +393,14 @@ class MainActivity : AppCompatActivity(), VideoClicked {
                 val videosJsonObject1 = videoJsonArray1.getJSONObject(0)
 
                 val stats = videosJsonObject1.getJSONObject("statistics")
-                val likes: String = try{
+                val likes: String = try {
                     stats.getString("likeCount")
-                } catch (e: Exception){
+                } catch (e: Exception) {
                     "-"
                 }
-                val views: String = try{
+                val views: String = try {
                     stats.getString("viewCount")
-                } catch (e: Exception){
+                } catch (e: Exception) {
                     "-"
                 }
 
@@ -391,7 +411,7 @@ class MainActivity : AppCompatActivity(), VideoClicked {
                 mAdapter.updateVide(lVArray)
             },
             {
-                Log.e("Error","something went wrong")
+                Log.e("Error", "something went wrong")
             })
         //Adding request to Singleton class
         MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest1)
